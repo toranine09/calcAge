a495157899b106f8572028be6ba68600a8985031
toranine09 <83958849+toranine09@users.noreply.github.com>
2024-02-18 16:41:14 +0000

    Update calcAge.js
    Overhauled variable names. Changed cake to class (previously id). Added bool for cake making. Commented setup. Put everything into functions.

--- calcAge.js
+++ calcAge.js
@@ -1,50 +1,86 @@
-const ageElementId = "agehere";
-const cakeElementId = "cakehere";
-const birthYear = 1999;
-const birthMonth = 9;
-const birthDay = 16;
-const ageFlavourText = [" years old"];
-const cakeFlavourText = [" 🎉", " 🎂", " 🍰", " today!"];
-const fallbackAgeText = "Born in " + birthYear;
-const fallbackCakeText = " today!";
+// user setup begins
+const ageBirthYear = 1999;
+const ageBirthMonth = 9;
+const ageBirthDay = 16;
+const ageTextElementId = "agehere"; // where to put calculated age text
+const ageFlavourText = [" years old"]; // appended to calculated age
+const ageFallbackAgeFlavourText = "Born in " + ageBirthYear;
+const ageMakeCake = true; // set to false to disable celebration text display
+const ageCakeTextClassName = "cakehere"; // where to put celebration text
+const ageCakeFlavourText = [" 🎉", " 🎂", " 🍰", " today!"]; // celebration text
+const ageFallbackCakeFlavourText = " today!";
+// end of user setup
 
-var ageElement = document.getElementById(ageElementId);
-var currentDate = new Date();
-var currentYear = currentDate.getFullYear();
-var currentMonth = currentDate.getMonth();
-var currentDay = currentDate.getDate();
-var calculatedNumericalAge = currentYear - birthYear;
+const ageCurrentDate = new Date();
+const ageCurrentYear = ageCurrentDate.getFullYear();
+const ageCurrentMonth = ageCurrentDate.getMonth();
+const ageCurrentDay = ageCurrentDate.getDate();
+var ageYearsElapsed = ageCurrentYear - ageBirthYear;
+var ageTextElement = document.getElementById(ageTextElementId);
+var ageCakeClassElements = document.getElementById(ageCakeTextClassName);
 
-if (ageElement) {
-  try {
-    if (currentMonth < birthMonth - 1) {
-      calculatedNumericalAge--;
-    }
-    if (birthMonth - 1 == currentMonth && currentDay < birthDay) {
-      calculatedNumericalAge--;
-    };
-    var randomAgeFlavour = ageFlavourText[Math.floor(Math.random() * ageFlavourText.length)];
-    ageElement.innerText = calculatedNumericalAge + randomAgeFlavour;
-  } catch (e) {
-    console.error("calcAge failed - " + e);
-    ageElement.innerText = fallbackAgeText;
+calcAgeInitialise();
+
+function calcAgeInitialise() {
+  var ageInitialiseFail = false;
+  if (ageTextElement == null) {
+    console.warn("calcAgeInitialise failed - could not locate ageTextElement by id '" + ageTextElementId + "'");
+    ageInitialiseFail = true;
   };
-} else {
-  console.error("calcAge failed - no element to insert age");
+
+  if (ageMakeCake && ageCakeTextClassName != null) {
+    var ageCakeInitialiseFail = false;
+    if (ageCakeClassElements == null) {
+      console.warn("calcAgeInitialise - could not locate ageCakeClassElements by id '" + ageCakeTextClassName + "'");
+      ageCakeInitialiseFail = true;
+    };
+  } else {
+    ageCakeInitialiseFail = true;
+  };
+
+  if (!ageInitialiseFail) {
+    doAgeFill();
+  } else {
+    console.error("calcAgeInitialise stopped - ageInitialiseFail");
+    return
+  };
+
+  if (!ageCakeInitialiseFail) {
+    makeCake();
+  } else {
+    console.error("calcAgeInitialise stopped - ageCakeInitialiseFail");
+  };
 };
 
-var cakeElement = document.getElementById(cakeElementId);
-if (cakeElement) {
+function doAgeFill() {
   try {
-    if (currentMonth == birthMonth - 1 && currentDay == birthDay) {
-      var randomCakeFlavour = cakeFlavourText[Math.floor(Math.random() * cakeFlavourText.length)];
-      cakeElement.innerText = randomCakeFlavour;
-      cakeElement.style.display = "inline";
+    if (ageCurrentMonth < ageBirthMonth - 1) {
+      ageYearsElapsed--;
+    }
+    if (ageBirthMonth - 1 == ageCurrentMonth && ageCurrentDay < ageBirthDay) {
+      ageYearsElapsed--;
+    };
+    var ageRandomFlavourText = ageFlavourText[Math.floor(Math.random() * ageFlavourText.length)];
+    ageTextElement.innerText = ageYearsElapsed + ageRandomFlavourText;
+  } catch (e) {
+    console.error("doAgeFill failed - " + e);
+    ageTextElement.innerText = ageFallbackAgeFlavourText;
+  };
+};
+
+function makeCake() {
+  try {
+    if (ageCurrentMonth == ageBirthMonth - 1 && ageCurrentDay == ageBirthDay) {
+      var ageRandomCakeFlavour = ageCakeFlavourText[Math.floor(Math.random() * ageCakeFlavourText.length)];
+      for (var i = 0; i < ageCakeClassElements.length; i++) {
+        ageCakeClassElements[i].innerText = ageRandomCakeFlavour;
+        ageCakeClassElements[i].style.display = "inline";
+      };
     };
   } catch (e) {
     console.error("makeCake failed - " + e);
-    cakeElement.innerText = fallbackCakeText;
+    for (var i = 0; i < ageCakeClassElements.length; i++) {
+      ageCakeClassElements[i].innerText = ageFallbackCakeFlavourText;
+    };
   };
-} else {
-  console.error("makeCake failed - no element to insert cake");
 };
